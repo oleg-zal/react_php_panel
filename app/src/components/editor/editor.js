52f15135eb9cdd4d201ee00b7d3495e577b6a44e
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-07-29 17:03:28 +0200

    step 8

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import React, {Component} from 'react';
 import DOMHelper from '../../helpers/dom-helper';
 import EditorText from '../editor-text';
+import UIkit from 'uikit';
+import Spinner from '../spinner';
 
 export default class Editor extends Component {
     constructor() {
@@ -10,9 +12,12 @@ export default class Editor extends Component {
         this.currentPage = "index.html";
         this.state = {
             pageList: [],
-            newPageName: ""
+            newPageName: "",
+            loading: true
         }
         this.createNewPage = this.createNewPage.bind(this);
+        this.isLoading = this.isLoading.bind(this);
+        this.isLoaded = this.isLoaded.bind(this);
     }
 
     componentDidMount() {
@@ -21,11 +26,11 @@ export default class Editor extends Component {
 
     init(page) {
         this.iframe = document.querySelector('iframe');
-        this.open(page);
+        this.open(page, this.isLoaded);
         this.loadPageList();
     }
 
-    open(page) {
+    open(page, cb) {
         this.currentPage = page;
 
         axios
@@ -40,15 +45,20 @@ export default class Editor extends Component {
             .then(html => axios.post("./api/saveTempPage.php", {html}))
             .then(() => this.iframe.load("../temp.html"))
             .then(() => this.enableEditing())
-            .then(() => this.injectStyles());
+            .then(() => this.injectStyles())
+            .then(cb);
     }
 
-    save() {
+    save(onSuccess, onError) {
+        this.isLoading();
         const newDom = this.virtualDom.cloneNode(this.virtualDom);
         DOMHelper.unwrapTextNodes(newDom);
         const html = DOMHelper.serializeDOMToString(newDom);
         axios
             .post("./api/savePage.php", {pageName: this.currentPage, html})
+            .then(onSuccess)
+            .catch(onError)
+            .finally(this.isLoaded);
     }
 
     enableEditing() {
@@ -95,31 +105,54 @@ export default class Editor extends Component {
             .catch(() => alert("Страницы не существует!"));
     }
 
+    isLoading() {
+        this.setState({
+            loading: true
+        })
+    }
+
+    isLoaded() {
+        this.setState({
+            loading: false
+        })
+    }
+
     render() {
-        // const {pageList} = this.state;
-        // const pages = pageList.map((page, i) => {
-        //     return (
-        //         <h1 key={i}>{page}
-        //             <a 
-        //             href="#"
-        //             onClick={() => this.deletePage(page)}>(x)</a>
-        //         </h1>
-        //     )
-        // });
+        const {loading} = this.state;
+        const modal = true;
+        let spinner;
+        
+        loading ? spinner = <Spinner active/> : spinner = <Spinner />
 
         return (
             <>
-                <button onClick={() => this.save()}>Click</button>
                 <iframe src={this.currentPage} frameBorder="0"></iframe>
+                
+                {spinner}
+
+                <div className="panel">
+                    <button className="uk-button uk-button-primary" uk-toggle="target: #modal-save">Опубликовать</button>
+                </div>
+                
+                <div id="modal-save" uk-modal={modal.toString()}>
+                    <div className="uk-modal-dialog uk-modal-body">
+                        <h2 className="uk-modal-title">Сохранение</h2>
+                        <p>Вы действительно хотите сохранить изменения?</p>
+                        <p className="uk-text-right">
+                            <button className="uk-button uk-button-default uk-modal-close" type="button">Отменить</button>
+                            <button 
+                                className="uk-button uk-button-primary uk-modal-close" 
+                                type="button"
+                                onClick={() => this.save(() => {
+                                    UIkit.notification({message: 'Успешно сохранено', status: 'success'})
+                                },
+                                () => {
+                                    UIkit.notification({message: 'Ошибка сохранения', status: 'danger'})
+                                })}>Опубликовать</button>
+                        </p>
+                    </div>
+                </div>
             </>
-            
-            // <>
-            //     <input
-            //         onChange={(e) => {this.setState({newPageName: e.target.value})}} 
-            //         type="text"/>
-            //     <button onClick={this.createNewPage}>Создать страницу</button>
-            //     {pages}
-            // </>
         )
     }
 }
