6a109a7e974682fff4d478838791f4be3b422012
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-07-30 18:33:15 +0200

    step 13

--- app/api/deleteTempPage.php
+++ app/api/deleteTempPage.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if ($_SESSION["auth"] != true) {
+    header("HTTP/1.0 403 Forbidden");
+    die;
+}
+
 $file = "../../yfuy1g221ub_hhg44.html";
 
 if (file_exists($file)) {
--- gulpfile.js
+++ gulpfile.js
@@ -48,21 +48,24 @@ gulp.task("build-sass", () => {
 });
 
 gulp.task("copy-api", () => {
+    gulp.src("./app/api/**/.*")
+        .pipe(gulp.dest(dist + "/api"));
+
     return gulp.src("./app/api/**/*.*")
                 .pipe(gulp.dest(dist + "/api"));
 });
 
 gulp.task("copy-assets", () => {
-    return gulp.src("./app/assets/!**!/!*.*")
+    return gulp.src("./app/assets/**/*.*")
                 .pipe(gulp.dest(dist + "/assets"));
 });
 
 gulp.task("watch", () => {
     gulp.watch("./app/src/index.html", gulp.parallel("copy-html"));
-    gulp.watch("./app/assets/!**!/!*.*", gulp.parallel("copy-assets"));
-    gulp.watch("./app/api/!**!/!*.*", gulp.parallel("copy-api"));
-    gulp.watch("./app/scss/!**!/!*.scss", gulp.parallel("build-sass"));
-    gulp.watch("./app/src/!**/!*.js", gulp.parallel("build-js"));
+    gulp.watch("./app/assets/**/*.*", gulp.parallel("copy-assets"));
+    gulp.watch("./app/api/**/*.*", gulp.parallel("copy-api"));
+    gulp.watch("./app/scss/**/*.scss", gulp.parallel("build-sass"));
+    gulp.watch("./app/src/**/*.js", gulp.parallel("build-js"));
 });
 
 gulp.task("build", gulp.parallel("copy-html", "copy-assets", "copy-api", "build-sass", "build-js"));
